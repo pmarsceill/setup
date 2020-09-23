0334f2ce6ca82b4f13d43fb0385959bdf9bb0060
Patrick Marsceill <pmarsceill@users.noreply.github.com>
2020-09-23 13:13:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-BASEDIR="$(cd "$dirname "${BASH_SOURCE[0]}")" && pwd)"
+brew install stow
 
 # symlink all dot files
 
-ln -s ${BASEDIR}/vim/* ~/
-ln -s ${BASEDIR}/alfred/* ~/
-ln -s ${BASEDIR}/bash/* ~/
-ln -s ${BASEDIR}/desktop-bg/* ~/
-ln -s ${BASEDIR}/git/* ~/
-ln -s ${BASEDIR}/irc/* ~/
-ln -s ${BASEDIR}/iterm/* ~/
-ln -s ${BASEDIR}/vim/* ~/
-ln -s ${BASEDIR}/zsh/* ~/
+$ stow -t "$HOME" "alfred"
+$ stow -t "$HOME" "bash"
+$ stow -t "$HOME" "desktop-bg"
+$ stow -t "$HOME" "git"
+$ stow -t "$HOME" "irc"
+$ stow -t "$HOME" "iterm"
+$ stow -t "$HOME" "vim"
+$ stow -t "$HOME" "zsh"
