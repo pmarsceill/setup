99f55dd2d1ef08fe15ba4bd2a968aac99bc21df9
Patrick Marsceill <pmarsceill@users.noreply.github.com>
2020-09-23 12:32:06 -0400

    Forgot this 😬

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ BASEDIR="$(cd "$dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # symlink all dot files
 
-ln -s ${BASEDIR}/vim/ ~/
-ln -s ${BASEDIR}/alfred/ ~/
-ln -s ${BASEDIR}/bash/ ~/
-ln -s ${BASEDIR}/desktop-bg/ ~/
-ln -s ${BASEDIR}/git/ ~/
-ln -s ${BASEDIR}/irc/ ~/
-ln -s ${BASEDIR}/iterm/ ~/
-ln -s ${BASEDIR}/vim/ ~/
-ln -s ${BASEDIR}/zsh/ ~/
+ln -s ${BASEDIR}/vim/* ~/
+ln -s ${BASEDIR}/alfred/* ~/
+ln -s ${BASEDIR}/bash/* ~/
+ln -s ${BASEDIR}/desktop-bg/* ~/
+ln -s ${BASEDIR}/git/* ~/
+ln -s ${BASEDIR}/irc/* ~/
+ln -s ${BASEDIR}/iterm/* ~/
+ln -s ${BASEDIR}/vim/* ~/
+ln -s ${BASEDIR}/zsh/* ~/
