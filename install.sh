f7b93a105bde0e4ed60962ef4a41a18f31674ecc
Patrick Marsceill <pmarsceill@users.noreply.github.com>
2020-09-23 13:14:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ brew install stow
 
 # symlink all dot files
 
-$ stow -t "$HOME" "alfred"
-$ stow -t "$HOME" "bash"
-$ stow -t "$HOME" "desktop-bg"
-$ stow -t "$HOME" "git"
-$ stow -t "$HOME" "irc"
-$ stow -t "$HOME" "iterm"
-$ stow -t "$HOME" "vim"
-$ stow -t "$HOME" "zsh"
+stow -t "$HOME" "alfred"
+stow -t "$HOME" "bash"
+stow -t "$HOME" "desktop-bg"
+stow -t "$HOME" "git"
+stow -t "$HOME" "irc"
+stow -t "$HOME" "iterm"
+stow -t "$HOME" "vim"
+stow -t "$HOME" "zsh"
