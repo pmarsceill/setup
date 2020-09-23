67c98a02ec3c6702aae3602abe94e0e60072bcc9
Patrick Marsceill <patrick.marsceill@gmail.com>
2020-09-23 15:18:20 -0400

    Add vim config

---
+++ config/nvim/init.vim
@@ -0,0 +1,5 @@
+let $M_TUI_ENABLE_TRUE_COLOR = 1
+
+let g:python_host_prog = '/usr/bin/python2.7'
+
+execute 'source' fnamemodify(expand('<sfile>'), ':h').'/../../.vimrc'
--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ brew install stow
 
 # symlink all dot files
 
+stow -t "$HOME/.oh-my-zsh" "oh-my-zsh" --ignore '.*\DS_Store'
+stow -t "$HOME/.config" "config" --ignore '.*\DS_Store'
+
 stow -t "$HOME" "alfred" --ignore '.*\DS_Store'
 stow -t "$HOME" "bash" --ignore '.*\DS_Store'
 stow -t "$HOME" "desktop-bg" --ignore '.*\DS_Store'
@@ -12,4 +15,3 @@ stow -t "$HOME" "irc" --ignore '.*\DS_Store'
 stow -t "$HOME" "iterm" --ignore '.*\DS_Store'
 stow -t "$HOME" "vim" --ignore '.*\DS_Store'
 stow -t "$HOME" "zsh" --ignore '.*\DS_Store'
-stow -t "$HOME/.oh-my-zsh" "oh-my-zsh" --ignore '.*\DS_Store'
